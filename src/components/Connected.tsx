a490db3ade08bb2583b89501358eee2d9f575405
Gianluigi Tiesi <sherpya@gmail.com>
2023-09-17 20:43:57 +0200

    added image when not connected

--- src/components/Connected.tsx
+++ src/components/Connected.tsx
@@ -1,10 +1,18 @@
 'use client';
 
 import { useAccount } from 'wagmi';
+import Image from 'next/image';
+
+import faucet from '@/public/faucet.webp';
 
 export function Connected({ children }: { children: React.ReactNode; }) {
   const { isConnected } = useAccount();
 
-  if (!isConnected) return null;
-  return <>{children}</>;
+  return isConnected ? <>{children}</> : (
+    <Image
+      className="mx-auto w-auto opacity-75"
+      src={faucet}
+      alt="Token Faucet"
+      width={1024} height={1024}
+    />);
 }
