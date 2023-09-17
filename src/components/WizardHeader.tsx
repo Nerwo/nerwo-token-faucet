f0e99ad867dc252ce18e6dfa054b1efbcdeb9f9a
Gianluigi Tiesi <sherpya@gmail.com>
2023-09-17 14:49:28 +0200

    fixed wizard header

--- src/components/WizardHeader.tsx
+++ src/components/WizardHeader.tsx
@@ -6,6 +6,28 @@ export function WizardHeader() {
     const { activeStep } = useWizard();
 
     return (
-        <div className='wizard-header'>Step {activeStep + 1}</div>
+        <div className='wizard-header'>
+            <ul>
+                <li>
+                    <span className="marker-number">✓</span>
+                    {activeStep > 0 && (<span className="marker-line"></span>)}
+                    <span className="marker-text">Connect</span>
+                </li>
+                <li>
+                    <span className="marker-number">{activeStep > 0 && '✓'}</span>
+                    {activeStep > 1 && (<span className="marker-line"></span>)}
+                    <span className="marker-text">Switch</span>
+                </li>
+                <li>
+                    <span className="marker-number">{activeStep > 1 && '✓'}</span>
+                    {activeStep > 2 && (<span className="marker-line"></span>)}
+                    <span className="marker-text">Mint</span>
+                </li>
+                <li>
+                    <span className="marker-number">{activeStep > 2 && '✓'}</span>
+                    <span className="marker-text">Done</span>
+                </li>
+            </ul>
+        </div>
     );
 }
