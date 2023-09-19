717fa57e3be85541250e2a84ae7a35f3fa64d43e
Gianluigi Tiesi <sherpya@gmail.com>
2023-09-19 13:52:19 +0200

    logout dropdown

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { BaseError } from 'viem';
 import Link from 'next/link';
+import { BaseError } from 'viem';
+import { useState } from 'react';
 import { useAccount, useConnect, useDisconnect, useEnsName } from 'wagmi';
 import { metamask } from '../wagmi';
 
@@ -12,15 +13,27 @@ function shortAddress(address: string | undefined) {
 export function Connect() {
   const { address, connector, isConnected } = useAccount();
   const { data: ensName } = useEnsName({ address });
-  const { connect, error, isLoading, pendingConnector } = useConnect({ connector: metamask});
+  const { connect, error, isLoading, pendingConnector } = useConnect({ connector: metamask });
   const { disconnect } = useDisconnect();
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropDown = () => setShowDropdown(!showDropdown);
 
   return (
     <div>
       {isConnected && (
-        <button onClick={() => disconnect()}>
-          {ensName ?? shortAddress(address)}
-        </button>
+        <div className="relative">
+          <button className="button-submit" onClick={toggleDropDown} type="button">
+            {ensName ?? shortAddress(address)}
+            <svg className="inline w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" /></svg>
+          </button>
+          <div className={`absolute p-2 ${showDropdown ? 'block' : 'hidden'}`}>
+            <button className="rounded text-primary bg-white border hover:bg-hover dark:hover:bg-hover"
+              onClick={() => { setShowDropdown(false); disconnect(); }}>
+              Disconnect
+            </button>
+          </div>
+        </div>
       )}
 
       {metamask.ready && connector?.id !== metamask.id && <button key={metamask.id} onClick={() => connect({ connector: metamask })}>
